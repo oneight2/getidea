2ac9d911973b80dedd4be287fdb9002574f7bd55
F13nd-RifanAlamsyah <rifan.alamsyah03@gmail.com>
2019-10-06 21:53:00 +0700

    login sistem

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,11 +1,34 @@
 <?php
 class Admin extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Admin_model');
+    }
 
     public function index()
     {
+
+        if (isset($_POST)) {
+            if ($this->Admin_model->login()) {
+                $this->session->set_userdata('login', true);
+                redirect('admin/dashboard');
+            }
+        }
+
+        $data['judul'] = 'Login';
         $this->load->view('templatesAdmin/header', $data);
-        $this->load->view('admin/index', $data);
+        $this->load->view('admin/index');
+        $this->load->view('templatesAdmin/footer');
+    }
+
+    public function dashboard()
+    {
+        $data['judul'] = 'Dashboard';
+        $this->load->view('templatesAdmin/header', $data);
+        $this->load->view('templatesAdmin/sidebar');
+        $this->load->view('admin/dashboard');
         $this->load->view('templatesAdmin/footer');
     }
 }
--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -6,7 +6,7 @@
 <div class="login-area login-s2">
     <div class="container">
         <div class="login-box ptb--100">
-            <form>
+            <form action="" method="post">
                 <div class="login-form-head">
                     <h4>getIdea</h4>
                     <p>Hello there, Sign in and start managing your website.</p>
@@ -14,16 +14,16 @@
                 <div class="login-form-body">
                     <div class="form-gp">
                         <label for="username">Username</label>
-                        <input type="text" id="username">
-                        <i class="ti-email"></i>
+                        <input type="text" id="username" name="username">
+                        <i class="ti-user"></i>
                     </div>
                     <div class="form-gp">
                         <label for="password">Password</label>
-                        <input type="password" id="password">
+                        <input type="password" id="password" name="password">
                         <i class="ti-lock"></i>
                     </div>
                     <div class="submit-btn-area">
-                        <button id="form_submit" type="submit">Submit <i class="ti-arrow-right"></i></button>
+                        <button id="form_submit" type="submit" name="login">Submit <i class="ti-arrow-right"></i></button>
                     </div>
                 </div>
             </form>
