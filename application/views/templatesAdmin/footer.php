997d7200d502e36476435db990666dbba60cf6be
oneight2 <1606109@sttgarut.ac.id>
2019-10-07 06:35:51 +0700

    rifan bala

--- application/views/templatesAdmin/footer.php
+++ application/views/templatesAdmin/footer.php
@@ -1,18 +1,6 @@
-<!-- login area end -->
-
-<!-- jquery latest version -->
-<script src="<?= base_url(); ?>assets/js/vendor/jquery-2.2.4.min.js"></script>
-<!-- bootstrap 4 js -->
-<script src="<?= base_url(); ?>assets/js/popper.min.js"></script>
-<script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
-<script src="<?= base_url(); ?>assets/js/owl.carousel.min.js"></script>
-<script src="<?= base_url(); ?>assets/js/metisMenu.min.js"></script>
-<script src="<?= base_url(); ?>assets/js/jquery.slimscroll.min.js"></script>
-<script src="<?= base_url(); ?>assets/js/jquery.slicknav.min.js"></script>
-
-<!-- others plugins -->
-<script src="<?= base_url(); ?>assets/js/plugins.js"></script>
-<script src="<?= base_url(); ?>assets/js/scriptsAdmin.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="<?= base_url(); ?>assets/js/scriptsAdmin.js"></script>
 </body>
-
 </html>
