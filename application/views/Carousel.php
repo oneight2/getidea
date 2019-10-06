13999d14a07625e228ed7939979666734760395e
omenology <ikbalsukabarang@gamil.com>
2019-10-06 17:21:03 +0700

    carousel

--- application/views/Carousel.php
+++ application/views/Carousel.php
@@ -1,23 +1,46 @@
-<div class="row no-gutters mx-n3">
-  <div class="col-5 p-5 pt-3 d-flex align-items-center">
-    <div class="deskripsi-carousel p-3">
-      <p class="judul">LOREM IP</p>
-      <h1 class="my-4">Good House</h1>
-      <p>Sed ut perspiciatis unde omnis iste natus ccusantium doloremque laudantium error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
-      <a href="">GO TO SITE</a>
+<div class="slide">
+  <div class="item">
+    <div class="slide-item" id="item1">
+      <div class="row no-gutters">
+        <div class="col-5 p-5 d-flex align-items-center">
+          <div class="info-slide p-3">
+            <h2 class="sub-judul">LOREM IP</h2>
+            <h1 class="my-4 judul">Good House</h1>
+            <p class="deskripsi-slide">Sed ut perspiciatis unde omnis iste natus ccusantium doloremque laudantium error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+            <a href="" class="info-btn">GO TO SITE</a>
+          </div>
+        </div>
+        <div class="col-7 slide-gambar">
+          <img class="w-100" src="https://images.pexels.com/photos/921294/pexels-photo-921294.png">
+        </div>
+      </div>
+    </div>
+    <div class="slide-item" id="item2">
+      <div class="row no-gutters">
+        <div class="col-5 p-5 d-flex align-items-center">
+          <div class="info-slide p-3">
+            <h2 class="sub-judul">Beach</h2>
+            <h1 class="my-4 judul">The Beach</h1>
+            <p class="deskripsi-slide">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+            <a href="" class="info-btn">GO TO SITE</a>
+          </div>
+        </div>
+        <div class="col-7 slide-gambar">
+          <img class="w-100" src="https://images.pexels.com/photos/1008732/pexels-photo-1008732.jpeg">
+        </div>
+      </div>
     </div>
   </div>
-  <div class="col-7 py-3">
-    <img class="w-100" src="https://images.pexels.com/photos/921294/pexels-photo-921294.png">
-  </div>
-</div>
-<div class="row justify-content-center">
-  <div class="col-6">
-    <div class="row no-gutters">
-      <div class="col-3"><img class="w-100 px-1" src="https://images.pexels.com/photos/991012/pexels-photo-991012.jpeg"></div>
-      <div class="col-3"><img class="w-100 px-1" src="https://images.pexels.com/photos/921294/pexels-photo-921294.png"></div>
-      <div class="col-3"><img class="w-100 px-1" src="https://images.pexels.com/photos/1029614/pexels-photo-1029614.jpeg"></div>
-      <div class="col-3"><img class="w-100 px-1" src="https://images.pexels.com/photos/1008732/pexels-photo-1008732.jpeg"></div>
+  <div class="indicator">
+    <div class="row justify-content-center slide-indicator">
+      <div class="col-6">
+        <div class="row no-gutters">
+          <div class="col-3" id="tes1"><img class="w-100 px-1" src="https://images.pexels.com/photos/1008732/pexels-photo-1008732.jpeg"></div>
+          <div class="col-3" id="tes2"><img class="w-100 px-1" src="https://images.pexels.com/photos/921294/pexels-photo-921294.png"></div>
+          <div class="col-3"><img class="w-100 px-1" src="https://images.pexels.com/photos/1029614/pexels-photo-1029614.jpeg"></div>
+          <div class="col-3"><img class="w-100 px-1" src="https://images.pexels.com/photos/1008732/pexels-photo-1008732.jpeg"></div>
+        </div>
+      </div>
     </div>
   </div>
 </div>
