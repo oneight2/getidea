76a68cb3765b0e8776a86affe568f6da5a7e82b2
oneight2 <1606109@sttgarut.ac.id>
2019-10-14 12:20:51 +0700

    halaman pemesanan

--- application/views/pemesanan.php
+++ application/views/pemesanan.php
@@ -20,33 +20,55 @@
             <input type="radio" id="tab4" name="tab-control">
             <ul>
               <li title="Features">
-                <label for="tab1" role="button"><i class="fa fa-check-double"></i><br><span>Features</span></label>
+                <label for="tab1" role="button">
+                  <i class="fa fa-comments"></i><br>
+                  <span>Diskusi Kebutuhan</span>
+                </label>
               </li>
               <li title="Delivery Contents">
-                <label for="tab2" role="button"><br><span>Delivery Contents</span></label>
+                <label for="tab2" role="button">
+                  <i class="fa fa-dollar"></i><br>
+                  <span>Administrasi</span>
+                </label>
               </li>
               <li title="Shipping">
-                <label for="tab3" role="button"><br><span>Shipping</span>
+                <label for="tab3" role="button">
+                  <i class="fa fa-cogs"></i><br>
+                  <span>Pembangunan</span>
               </label>
             </li>    <li title="Returns">
-            <label for="tab4" role="button"><br><span>Returns</span></label>
+            <label for="tab4" role="button">
+              <i class="fa fa-check"></i><br>
+              <span>Launching & Pemeliharaan</span>
+            </label>
           </li>
         </ul>
         
         <div class="slider"><div class="indicator"></div></div>
         <div class="content">
           <section>
-            <h2>Features</h2>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea dolorem sequi, quo tempore in eum obcaecati atque quibusdam officiis est dolorum minima deleniti ratione molestias numquam. Voluptas voluptates quibusdam cum?</section>
+            <h2>Diskusi Kebutuhan</h2>
+            <p>
+              Berbagai informasi yang berhubungan dengan aktivitas konvensional yang ingin anda ubah menjadi sistem digital berplatform websiste, disiapkan dengan sebaik baiknya, karena kami akan menggunakan informasi tersebut sebagai dasar algoritma kami. <br>
+              Hambatan atau masalah yang sering ditemui dalam aktivitas konvensional, kita diskusikan bagaimana hambatan atau masalah tersebut supaya tidak ditemui lagi dalam sistem diigital berplatfrom websiet, dengan cara mengumpulkan informasi tentang hambatan atau masalah tersebut, kemudian akan kami manipulasi. <br>
+              Dalam diskusi kebutuhan ini, kami akan membantu anda untuk sama sama menggambarkan bagaimana aktivitas konvensioanal jika sudah berubah menjadi sitem digital berplatfrom website, sehingga kebutuhan kebutuhan yang anda inginkan akan jelas.
+            </p>
+          </section>
           <section>
-            <h2>Delivery Contents</h2>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem quas adipisci a accusantium eius ut voluptatibus ad impedit nulla, ipsa qui. Quasi temporibus eos commodi aliquid impedit amet, similique nulla.</section>
+            <h2>Administrasi</h2>
+            Setelah semua rancangan kebutuhan sudah terkumpul, selanjutnya kami akan menawarkan harga sesuai dengan rancangan kebutuhan yang sudah disepakati, jika harga tidak sesuai dengan budget anda, kita diskusikan kembali rancangan kebutuhan agar bisa mengikuti dengan budget anda.
+            Semua proses diatas sudah beres, kemudian kita atur administrasi transaksi, dari mulai laporan kerja, pembayaran, hingga kontrak pemeliharaan setelah sistem digital sudah selesai dibangun.
+          </section>
           <section>
-            <h2>Shipping</h2>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam nemo ducimus eius, magnam error quisquam sunt voluptate labore, excepturi numquam! Alias libero optio sed harum debitis! Veniam, quia in eum.</section>
+            <h2>Pembangunan</h2>
+            Dalam pembangunan sistem digital berplatform website ini, kami akan selalu melibatkan anda, karena kepuasan anda adalah hal yang paling utama bagi kami, dengan terlibatnya anda dalam pembangunan ini, tentu semua yang anda inginkan sebelumnya akan terus terkontrol.
+            Untuk waktu pembangunannya tergantung dari seberapa banyak rancangan kebutuhan yang sudah disepakati, dan dari seberapa sulit hambatan atau masalah yang ada pada aktivitas konvensional sehingga harus diselesaikan dalam sistem digital ini.
+          </section>
           <section>
-            <h2>Returns</h2>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dicta vero rerum? Eaque repudiandae architecto libero reprehenderit aliquam magnam ratione quidem? Nobis doloribus molestiae enim deserunt necessitatibus eaque quidem incidunt.</section>
+            <h2>Launching & Pemeliharaan </h2>
+            Setelah pembangunan sitem digital berplatform website ini selesai, sebelum melanjutkan ketahap berikutnya yaitu launching, terlebih dahulu anda harus menyelesaikan semua biaya.
+            Pemeliharaan sistem digital ini akan kita lakukan untuk mengoptimalkan kinerja dari sistem digital tersebut.
+          </section>
         </div>
       </div>
     </div>
