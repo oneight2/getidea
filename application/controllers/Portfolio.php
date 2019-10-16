52286055bbfcc34c11cf54237e0272c16b3742a9
F13nd-RifanAlamsyah <rifan.alamsyah03@gmail.com>
2019-10-16 14:13:52 +0700

    slideshow di asupan sesuatu

--- application/controllers/Portfolio.php
+++ application/controllers/Portfolio.php
@@ -1,12 +1,15 @@
-<?php 
-class Portfolio extends CI_Controller {
+<?php
+class Portfolio extends CI_Controller
+{
 
 	public function index()
 	{
+		$this->load->model('Portfolio_model');
+		$data['aplikasi'] = $this->Portfolio_model->getAll('aplikasi');
+
 		$data['judul'] = 'Portfolio';
 		$this->load->view('templates/header', $data);
 		$this->load->view('Portfolio');
 		$this->load->view('templates/footer');
-
 	}
 }
