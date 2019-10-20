d1eb01c6283c7c9c6cab12c206bdbbe3c207cbb8
F13nd-RifanAlamsyah <rifan.alamsyah03@gmail.com>
2019-10-20 13:00:29 +0700

    beres detail

--- application/views/admin/detail_komplain.php
+++ application/views/admin/detail_komplain.php
@@ -1,16 +1,41 @@
-<h1>Screenshoot</h1>
-<div class="slider">
-    <?php foreach ($detail as $row) : ?>
-        <div class="box"><a href="<?= base_url() ?>img/komplain/<?= $row['gambar']; ?>"><img src="<?= base_url() ?>img/komplain/<?= $row['gambar']; ?>" /></a></div>
-    <?php endforeach; ?>
-</div>
-<div class="menu">
-    <?php
-    $i = 1;
-    foreach ($detail as $row) : ?>
-        <a href="#" data-picindex="<?= $i; ?>">
-            <img src="<?= base_url() ?>img/komplain/<?= $row['gambar']; ?>" style="height:40px;width:auto">
-        </a>
-    <?php $i++;
-    endforeach; ?>
+<div class="container my-2">
+    <div class="row">
+        <div class="col-4">
+            <div class="card text-white bg-info">
+                <div class="card-header">
+                    <h4 class="text-center"><?= $komplain['website'] ?> </h4>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Nama : <?= $komplain['nama_client'] ?></li>
+                    <li class="list-group-item">Telp : <?= $komplain['nomor_client'] ?></li>
+                    <li class="list-group-item">Email : <?= $komplain['email'] ?></li>
+                </ul>
+                <div class="card-body ">
+                    <div class="card-title">Detail Komplain</div>
+                    <div class="card-text ">
+                        <p class="text-justify text-white"><?= $komplain['komplain'] ?> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-8">
+            <div class="slider">
+                <?php foreach ($detail as $row) : ?>
+                    <div class="box"><a href="<?= base_url() ?>img/komplain/<?= $row['gambar']; ?>"><img class="mx-auto d-block" src="<?= base_url() ?>img/komplain/<?= $row['gambar']; ?>" style="height:30em" /></a></div>
+                <?php endforeach; ?>
+            </div>
+            <div class="menu">
+
+                <?php
+                $i = 1;
+                foreach ($detail as $row) : ?>
+                    <a href="#" data-picindex="<?= $i; ?>">
+                        <img src="<?= base_url() ?>img/komplain/<?= $row['gambar']; ?>" style="height:60px;width:auto">
+                    </a>
+                <?php $i++;
+                endforeach; ?>
+            </div>
+        </div>
+    </div>
+
 </div>
